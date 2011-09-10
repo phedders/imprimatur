6ef96c8e5bcc8dbb308dcc29f0cf7e57085f0f74
Sergey Poznyakoff <gray@gnu.org.ua>
2011-09-10 14:27:57 +0300

    Initial commit.

---
+++ imprimatur.m4
@@ -0,0 +1,49 @@
+# imprimatur.m4 serial 1
+dnl This file is part of Imprimatur.
+dnl Copyright (C) 2011 Sergey Poznyakoff
+dnl
+dnl Imprimatur is free software; you can redistribute it and/or modify
+dnl it under the terms of the GNU General Public License as published by
+dnl the Free Software Foundation; either version 3, or (at your option)
+dnl any later version.
+dnl
+dnl Imprimatur is distributed in the hope that it will be useful,
+dnl but WITHOUT ANY WARRANTY; without even the implied warranty of
+dnl MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+dnl GNU General Public License for more details.
+dnl
+dnl You should have received a copy of the GNU General Public License
+dnl along with Imprimatur.  If not, see <http://www.gnu.org/licenses/>.
+
+# IMPRIMATUR_INIT([DIR],[RENDITION])
+# DIR       - Directory in the source tree where imprimatur has been cloned.
+#             Default is "imptimatur".
+# RENDITION - Documentation rendition.  Default is DISTRIB for stable releases
+#             and PROOF for alpha releases.  The release type is determined by
+#             the version number, assuming GNU versioning.
+AC_DEFUN([IMPRIMATUR_INIT],[
+ m4_pushdef([imprimaturdir],[m4_if([$1],,[imprimatur],[$1])])
+ AC_SUBST([IMPRIMATUR_MODULE_DIR],imprimaturdir)
+ AC_CONFIG_FILES(imprimaturdir[/Makefile])
+ AC_SUBST(RENDITION)
+ m4_if([$2],,[
+  # Doc hints.
+  # Select a rendition level:
+  #  DISTRIB for stable releases (at most one dot in the version number)
+  #  and maintenance releases (two dots, patchlevel < 50)
+  #  PROOF for alpha releases.
+  #  PUBLISH can only be required manually when running make in doc/
+  case `echo $VERSION|sed  's/[[^.]]//g'` in
+  ""|".")  RENDITION=DISTRIB;;
+  "..")  if test `echo $VERSION | sed  's/.*\.//'` -lt 50; then
+	   RENDITION=DISTRIB
+         else
+           RENDITION=PROOF
+         fi;;
+  *)     RENDITION=PROOF;;
+  esac
+ ],[
+  RENDITION=$2
+ ])
+ m4_popdef([imprimaturdir])
+])
---
+++ imprimatur.mk
@@ -0,0 +1,86 @@
+# This file is part of Imprimatur.
+# Copyright (C) 2006, 2007, 2010, 2011 Sergey Poznyakoff
+#
+# Imprimatur is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3, or (at your option)
+# any later version.
+#
+# Imprimatur is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Imprimatur.  If not, see <http://www.gnu.org/licenses/>.
+
+imprimatur-format:
+	@if test -n "`cat $(imprimatur_INPUT) | tr -d -c '\t'`"; then \
+		echo "Sources contain tabs; run make imprimatur-untabify"; \
+		false; \
+	fi
+
+imprimatur-refs:
+	@for file in $(imprimatur_INPUT); \
+	do \
+	  sed -e = $$file | \
+           sed -n 'N;/@FIXME-.*ref/{s/\(^[0-9][0-9]*\).*@FIXME-.*ref{\([^}]*\)}.*/'$$file':\1: \2/gp}'; \
+	done > $@-t; \
+	if [ -s $@-t ]; then \
+	  echo "Unresolved cross-references:"; \
+	  cat $@-t;\
+	  rm $@-t; \
+	else \
+	  rm -f $@-t; \
+	fi
+
+imprimatur-fixmes:
+	@for file in $(imprimatur_INPUT); \
+	do \
+	  sed -e = $$file | \
+           sed -n 'N;/@FIXME{/{s/\(^[0-9][0-9]*\).*@FIXME{\([^}]*\).*/'$$file':\1: \2/gp}'; \
+	done > $@-t; \
+	if [ -s $@-t ]; then \
+	  echo "Unresolved FIXMEs:"; \
+	  cat $@-t; \
+	  rm $@-t; \
+	  false; \
+	else \
+          rm -f $@-t; \
+	fi
+
+imprimatur-writemes:
+	@grep -Hn @WRITEME $(imprimatur_INPUT) > $@-t; \
+	if [ -s $@-t ]; then \
+	  echo "Empty nodes:"; \
+	  cat $@-t; \
+	  rm $@-t; \
+	  false;\
+	else \
+          rm $@-t; \
+	fi
+
+imprimatur-unrevised:
+	@grep -Hn @UNREVISED $(imprimatur_INPUT) > $@-t; \
+	if [ -s $@-t ]; then \
+	  echo "Unrevised nodes:"; \
+	  cat $@-t; \
+	  rm $@-t; \
+	  false;\
+	else \
+          rm $@-t; \
+	fi
+
+imprimatur-basic-checks: imprimatur-format imprimatur-refs imprimatur-fixmes \
+                 imprimatur-writemes imprimatur-unrevised
+
+imprimatur-master-menu:
+	@emacs -batch -l $(IMPRIMATUR_MODULE_DIR)/mastermenu.el -f make-master-menu $(imprimatur_INPUT)
+
+imprimatur-untabify:
+	@emacs -batch -l $(IMPRIMATUR_MODULE_DIR)/untabify.el $(imprimatur_INPUT)
+
+imprimatur-final: imprimatur-untabify imprimatur-master-menu
+
+
+
